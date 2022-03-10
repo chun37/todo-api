a85fa726c104bc78765fb6fe0e738ac60f2eab95
chun37 <chun.jb.37@gmail.com>
2022-03-10 11:55:19 +0900

    swagger導入

--- todo/urls.py
+++ todo/urls.py
@@ -14,6 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("tasks/", include("task.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("tasks/", include("task.urls")),
+    path("swagger/", include("swagger.urls")),
+]
