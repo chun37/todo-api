6720283d3756f98d99e14b1d9313c9b3bb4f2172
chun37 <chun.jb.37@gmail.com>
2022-03-24 12:57:51 +0900

    isort

--- user/authentication.py
+++ user/authentication.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomTokenAuthentication(TokenAuthentication):
